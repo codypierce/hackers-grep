2f6053217af64a5fff0389fa6a0b5c6a863b2aa9
cody <codyrpierce@gmail.com>
2018-07-07 17:10:01 -0500

    fix cwd bug

--- file_details.py
+++ file_details.py
@@ -3,24 +3,28 @@ import os
 import win32com.client
 
 # https://technet.microsoft.com/library/ee176615.aspx
+
+
 def getFileDetails(abs_path):
-	details = {}
-	base_dir = os.path.dirname(abs_path)
-	file_name = os.path.basename(abs_path)
+    details = {}
+    base_dir = os.path.dirname(abs_path)
+    file_name = os.path.basename(abs_path)
 
-	sh = win32com.client.Dispatch("Shell.Application")
-	ns = sh.NameSpace(base_dir)
+    if base_dir == "." or base_dir == "":
+        base_dir = os.getcwd()
 
-	try:
-		idx = ns.ParseName(file_name)
-	except:
-		return None
+    sh = win32com.client.Dispatch("Shell.Application")
+    ns = sh.NameSpace(base_dir)
 
-	try:
-		details["vendor"] = ns.GetDetailsOf(idx, 33)
-		details["description"] = ns.GetDetailsOf(idx, 34)
-	except:
-		return None
+    try:
+        idx = ns.ParseName(file_name)
+    except:
+        return None
 
-	return details
+    try:
+        details["vendor"] = ns.GetDetailsOf(idx, 33)
+        details["description"] = ns.GetDetailsOf(idx, 34)
+    except:
+        return None
 
+    return details
